d85297727f48554454efbdf3a521088df82fe9c6
Randall Knutson <randall@form.io>
2018-01-11 11:33:08 -0600

    Make datagrids submit minimum number of rows so validations show errors for fields instead of datagrid.

--- functions/validateForm.js
+++ functions/validateForm.js
@@ -1,7 +1,27 @@
+const formioUtils = require('formiojs/utils');
+const _ = require('lodash');
 const request = require('request');
 const config = require('../config');
 
-module.exports = (url, data) => {
+module.exports = (url, data, components) => {
+  const submitData = _.clone(data);
+  // Change the way that datagrid validation works by making sure any required rows are added. This wil make fields on
+  // those rows return errors instead of the datagrid itself.
+  formioUtils.eachComponent(components, (component, path) => {
+    if (component.type === 'datagrid' && component.validate && component.validate.minLength > 0) {
+      let value = _.get(submitData, path, []);
+      if (!Array.isArray(value)) {
+        value = [];
+      }
+      if (value.length < component.validate.minLength) {
+        for (let i = value.length; i < component.validate.minLength; i++) {
+          value.push({});
+        }
+      }
+      _.set(submitData, 'data.' + path, value);
+    }
+  });
+
   return new Promise((resolve, reject) => {
     request({
       method: 'POST',
@@ -12,7 +32,7 @@ module.exports = (url, data) => {
       headers: {
         'x-token': config.apiKey
       },
-      json: data
+      json: submitData
     }, (error, response, body) => {
       if (error) {
         return reject(error);
